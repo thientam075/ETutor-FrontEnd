ef13140815e8c40d1704cada5d88fa5426d68462
HuyThien <57531802+HuyThien@users.noreply.github.com>
2022-01-09 13:45:40 +0700

    map API register page

--- etutor/configs/API.js
+++ etutor/configs/API.js
@@ -2,6 +2,7 @@ const API_ROOT = 'http://localhost:1337/api';
 
 export const API = {
   USER: {
+    REGISTER: API_ROOT+ '/nguoi-dungs/?',
     LIST: API_ROOT + '/nguoi-dungs/?',
     MANAGE_ACCESS: (id) => API_ROOT + `/nguoi-dungs/${id}`,
   },
