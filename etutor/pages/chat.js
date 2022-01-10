f10640bf75fa89389d32206f36abe2005ae8bebc
ThinhNguyenHuu <thinhnguyenhuu92@gmail.com>
2022-01-10 21:05:35 +0700

    add react-chat-component

---
+++ etutor/pages/chat.js
@@ -0,0 +1,45 @@
+import Navbar from "../components/navbar";
+import PubNub from "pubnub";
+import { PubNubProvider } from "pubnub-react";
+import {
+  Chat,
+  MessageList,
+  MessageInput,
+} from "@pubnub/react-chat-components";
+import { useState, useEffect } from "react";
+
+const pubnub = new PubNub({
+  publishKey: "pub-c-0e95f14b-ce30-45f0-9cc3-4477d5685559",
+  subscribeKey: "sub-c-b418c8f4-721a-11ec-97ad-065127b61789",
+  uuid: "123",
+});
+const currentChannel = "myCurrentChannel";
+const theme = "light";
+
+const ChatComponent = ({ channel }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
+  return (
+    <>
+      <div className="vh-100">
+        <Navbar />
+        <div style={{ height: '80%' }}>
+          {mounted && (
+            <PubNubProvider client={pubnub}>
+              <Chat {...{ currentChannel, theme }}>
+                <MessageList fetchMessages={10}/>
+                <MessageInput />
+              </Chat>
+            </PubNubProvider>
+          )}
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default ChatComponent;
