725b8b664a520865844313bf77ebf81ecb24747c
DangThanh2010 <57797011+DangThanh2010@users.noreply.github.com>
2022-01-10 20:28:31 +0700

    Change default avatar

--- etutor/components/infoTutor.js
+++ etutor/components/infoTutor.js
@@ -41,7 +41,7 @@ export default function InfoTutor({
       <div className="d-flex justify-content-around m-3">
         <div className="d-flex">
           <img
-            src={avatar ? avatar :"https://www.w3schools.com/howto/img_avatar.png"}
+            src={avatar ? avatar :"https://res.cloudinary.com/dangthanh/image/upload/v1641804706/AvatarEtutor/user_ryrffo.png"}
             className="avatarDetail"
           ></img>
           <div className="m-4 ps-2">
